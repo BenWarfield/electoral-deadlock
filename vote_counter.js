021febe2db83eeb473c2b5ffe946cae27e66ac3b
Benjamin Warfield <benwarfield@gmail.com>
2016-10-30 21:10:57 -0400

    Added summary information below map.
    Added HTML to show the current score and winner (if any), so the
    tally can be seen without going into the console.

--- vote_counter.js
+++ vote_counter.js
@@ -1,4 +1,5 @@
 (function($) {
+
     var collate_reps = function(replist) {
 		var states = {};
 		for (var i = 0; i < replist.length; i++) {
@@ -11,7 +12,8 @@
 		}
 		return states;
 	};
-	
+
+
 	var color_state = function(state_code, rep_list) {
 		var dcount = 0;
 		var rcount = 0;
@@ -32,6 +34,8 @@
 			partyLabel = "R"
 		}
 		var trow = $("<tr><th>"+ state_code + "</th><td>" + rcount + "<td>" + dcount + "<td>" + icount + "</tr>");
+		trow.on('click', function(e) {alert($(this).data("state"));});
+		trow.data("state", state_code);
 		$("#tally tbody").append(trow);
 
 		document.getElementById(state_code).className.baseVal = partyClass;
@@ -41,6 +45,7 @@
 	var state_reps = collate_reps(window.REPS);
 	var dstates = 0;
 	var rstates = 0;
+	var ostates = 0;
 	var state_codes = [];
 	for (var s in state_reps) state_codes.push(s);
 	state_codes.sort();
@@ -52,9 +57,20 @@
 	    	dstates++;
 	    } else if ("R" === foundParty) {
 	    	rstates++;
+	    } else if ("O" === foundParty) {
+			ostates++;
 	    }
 
 	}
+	$("#trump-state-count").text(rstates);
+	$("#clinton-state-count").text(dstates);
+	$("#mcmullin-state-count").text(ostates);
+	$("#top-line-summary .elected").hide()
+	if (rstates > 25) $("#trump-elected").show();
+	else if (dstates > 25) $("#clinton-elected").show();
+	else if (ostates > 25) $("#mcmullin-elected").show();
+	else $("#deadlock-elected").show()
+	
 	console.log("Final score: D", dstates, ", R", rstates);
 	
 })(jQuery);

d6c7a6d5a18523a221bc02dc769b5198be3c1b6a
Benjamin Warfield <benwarfield@gmail.com>
2016-10-30 21:32:10 -0400

    Refactored display code.
    Made the code that generates the table and colors the states somewhat
    easier to reuse when we have the ability to change the underlying data.

--- vote_counter.js
+++ vote_counter.js
@@ -14,6 +14,17 @@
 	};
 
 
+	var init_tally_display = function(state_codes) {
+		for (var i = 0; i < state_codes.length; i++) {
+		console.log("Adding row for " + state_codes[i]);
+		    var code = state_codes[i];
+			var trow = $("<tr id='tally-" + code + "'><th>"+ code +
+				"</th><td class='rcount'><td class='dcount'><td class='ocount'></tr>");
+			trow.data("state", code);
+			$("#tally tbody").append(trow);
+		}
+	};
+
 	var color_state = function(state_code, rep_list) {
 		var dcount = 0;
 		var rcount = 0;
@@ -33,44 +44,48 @@
 			partyClass = "redstate";
 			partyLabel = "R"
 		}
-		var trow = $("<tr><th>"+ state_code + "</th><td>" + rcount + "<td>" + dcount + "<td>" + icount + "</tr>");
-		trow.on('click', function(e) {alert($(this).data("state"));});
-		trow.data("state", state_code);
-		$("#tally tbody").append(trow);
+		var tally_row = $("#tally-" + state_code);
+		tally_row.find(".rcount").text(rcount);
+		tally_row.find(".dcount").text(dcount);
+		tally_row.find(".ocount").text(icount);
 
 		document.getElementById(state_code).className.baseVal = partyClass;
 		return partyLabel;
 	}
 
+	var tally_scores = function(state_reps) {
+		var dstates = 0;
+		var rstates = 0;
+		var ostates = 0;
+		for (var i = 0; i < state_codes.length; i++) {
+			var s = state_codes[i];
+			console.log("State", s, " has ", state_reps[s].length);
+			var foundParty = color_state(s, state_reps[s])
+			if ("D" === foundParty) {
+				dstates++;
+			} else if ("R" === foundParty) {
+				rstates++;
+			} else if ("O" === foundParty) {
+				ostates++;
+			}
+		}
+		$("#trump-state-count").text(rstates);
+		$("#clinton-state-count").text(dstates);
+		$("#mcmullin-state-count").text(ostates);
+		$("#top-line-summary .elected").hide()
+		if (rstates > 25) $("#trump-elected").show();
+		else if (dstates > 25) $("#clinton-elected").show();
+		else if (ostates > 25) $("#mcmullin-elected").show();
+		else $("#deadlock-elected").show()
+
+		console.log("Final score: D", dstates, ", R", rstates);
+	};
+
 	var state_reps = collate_reps(window.REPS);
-	var dstates = 0;
-	var rstates = 0;
-	var ostates = 0;
 	var state_codes = [];
 	for (var s in state_reps) state_codes.push(s);
 	state_codes.sort();
-	for (var i = 0; i < state_codes.length; i++) {
-		var s = state_codes[i];
-	    console.log("State", s, " has ", state_reps[s].length);
-		var foundParty = color_state(s, state_reps[s])
-	    if ("D" === foundParty) {
-	    	dstates++;
-	    } else if ("R" === foundParty) {
-	    	rstates++;
-	    } else if ("O" === foundParty) {
-			ostates++;
-	    }
-
-	}
-	$("#trump-state-count").text(rstates);
-	$("#clinton-state-count").text(dstates);
-	$("#mcmullin-state-count").text(ostates);
-	$("#top-line-summary .elected").hide()
-	if (rstates > 25) $("#trump-elected").show();
-	else if (dstates > 25) $("#clinton-elected").show();
-	else if (ostates > 25) $("#mcmullin-elected").show();
-	else $("#deadlock-elected").show()
-	
-	console.log("Final score: D", dstates, ", R", rstates);
+	init_tally_display(state_codes);
+	tally_scores(state_reps);
 	
 })(jQuery);
